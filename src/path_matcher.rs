3139839896d4f05fc7c65bedceac4fee59bf95a0
Wed0n <wed0n1213@gmail.com>
2026-02-20 09:13:42 +0800

    fix: add tmp folder

--- src/path_matcher.rs
+++ src/path_matcher.rs
@@ -6,17 +6,18 @@ use path_tree::PathTree;
 use crate::config::Config;
 
 static INNER_WHITELIST: &[&str] = &[
-    "/dev/null",
-    "/dev/zero",
-    "/dev/random",
-    "/dev/urandom",
     "/dev/dtracehelper",
+    "/dev/null",
     "/dev/ptmx",
+    "/dev/random",
     "/dev/tty*",
-    "/private/var/run/utmpx",
-    "/private/var/tmp/+",
+    "/dev/urandom",
+    "/dev/zero",
+    "/private/tmp/+",
     "/private/var/folders/_l/+",
+    "/private/var/run/utmpx",
     "/private/var/spool/+",
+    "/private/var/tmp/+",
 ];
 
 fn iter_slice<T: AsRef<str>>(tree: &mut PathTree<()>, slice: &[T]) -> Result<()> {
@@ -33,7 +34,7 @@ fn iter_slice<T: AsRef<str>>(tree: &mut PathTree<()>, slice: &[T]) -> Result<()>
         } else {
             real_pattern = pattern;
         }
-        log::debug!("add {} to glob set", real_pattern);
+        log::debug!("add {} to path matcher", real_pattern);
         let _ = tree.insert(real_pattern, ());
     }
 
@@ -49,7 +50,9 @@ impl PathMatcher {
         let mut tree = PathTree::<()>::new();
         let mut cwd = env::current_dir()?;
         cwd.push("+");
-        let cwd = cwd.to_str().ok_or(anyhow!("add cwd to glob set failed"))?;
+        let cwd = cwd
+            .to_str()
+            .ok_or(anyhow!("add cwd to path matcher failed"))?;
         let _ = tree.insert(cwd, ());
         iter_slice(&mut tree, INNER_WHITELIST)?;
         iter_slice(&mut tree, config.whitelist.as_slice())?;
